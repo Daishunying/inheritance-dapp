ae604fef50f0aa804df77dbeb9ab609bf097ab53
Daishunying <152150918+Daishunying@users.noreply.github.com>
2025-03-22 21:17:25 +0000

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import InheritanceDApp from "./InheritanceDApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <InheritanceDApp />
+  </React.StrictMode>
+);
